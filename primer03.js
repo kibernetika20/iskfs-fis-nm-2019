f4fa8c485c3385c0a957a15f399504b7b42606a8
kibernetika20 <kibernetika20@gmail.com>
2019-02-28 10:28:17 +0000

    primeri četrtek 28.2.2019  začetek

--- primer03.js
+++ primer03.js
@@ -12,31 +12,29 @@ http.createServer(function(req, res){ // http.createServer([requestListener])
                                       // "requestListener" je funkcija, ki se avtomatsko doda
                                       // k dogodku 'request'.
     var deli = req.url.split("/"); // razdelimo url glede na znak "/"
-    var operator = parseInt(deli[1],10); // 10 osnova številskega sistema, decimalno (od 2 do 36)
-                                     // npr. parseInt("ff", 16) vrne 255 | ("100", 2) -> 4
-    if (operator == 0) {
+    var operator1 = parseInt(deli[1],10); // 10 osnova številskega sistema, decimalno (od 2 do 36)
+    var operator2 = parseInt(deli[2],10); // 10 osnova številskega sistema, decimalno (od 2 do 36)
+    if (operator1 == 0) {
         console.log("Izključevanje LED1");
         board.digitalWrite(13, board.LOW);
     }
-    if (operator == 1) {
+    if (operator1 == 1) {
         console.log("Vključevanje LED1");
         board.digitalWrite(13, board.HIGH);
-        console.log(deli[0]);
-        console.log(deli[1]);
     }
     
-    if (operator == 2) {
+    if (operator2 == 0) {
         console.log("Izključevanje LED2");
         board.digitalWrite(8, board.LOW);
     }
 
-    if (operator == 3) {
+    if (operator2 == 1) {
         console.log("Vključevanje LED2");
         board.digitalWrite(8, board.HIGH);
     }                
         
     res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
-    res.write("Za test vpišite v brskalnikovo vrstico z naslovom: http://192.168.1.136:8080/1 <br>");
-    res.write("ali: http://192.168.1.136:8080/0<br>");
-    res.end("Vrednost operatorja: " + operator);
+    res.write("Za test vpišite v brskalnikovo vrstico z naslovom: http://192.168.1.136:8080/1/1 <br>");
+    res.write("ali: http://192.168.1.136:8080/0/0<br>");
+    res.end("Vrednost operatorja1: " + operator1 + "<br>" + "Vrednost operatorja2: " + operator2);
 }).listen(8080, "192.168.1.136");
